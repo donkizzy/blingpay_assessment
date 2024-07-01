65ba210ed25550480dc5acc08e486cbbf7e90714
Amos Godwin <amosgodwin500@gmail.com>
2024-07-01 01:09:47 +0100

    added tests to repository

---
+++ test/features/post/data/repositories/post_repository_test.dart
@@ -0,0 +1,49 @@
+import 'package:blingpay_assesment/features/post/data/repositories/post_repository.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+
+void main() {
+
+  late PostRepository postRepository ;
+  late FakeFirebaseFirestore firebaseFirestore ;
+  
+  setUp((){
+    firebaseFirestore = FakeFirebaseFirestore();
+    postRepository = PostRepository(firebaseFireStore: firebaseFirestore);
+  });
+
+  test('fetchPosts returns empty list when no documents found', () async {
+    final posts = await postRepository.fetchPosts();
+
+    expect(posts, isEmpty);
+  });
+
+  test('fetchPosts returns list of PostModel from documents', () async {
+    final List<Map<String, dynamic>> mockDocs = [
+      {
+        "thumbnail": "https://firebasestorage.googleapis.com/v0/b/blinqpost.appspot.com/o/blinqpayapp_283378730_5263452000387376_8497039804548532757_n.jpg?alt=media&token=445db31e-533b-4feb-b2dc-a7b7950a8ae9",
+        "userId": "JfpBYheu4zLhNMv539OR",
+        "id": "2",
+        "no_media": false,
+        "link": "https://firebasestorage.googleapis.com/v0/b/blinqpost.appspot.com/o/blinqpayapp_283378730_5263452000387376_8497039804548532757_n.jpg?alt=media&token=445db31e-533b-4feb-b2dc-a7b7950a8ae9",
+        "username": "Alex",
+        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.",
+        "timestamp": 1653513552,
+        "video": false
+      }
+    ];
+
+    for (var doc in mockDocs) {
+      await firebaseFirestore.collection('post').add(doc);
+    }
+
+    final posts = await postRepository.fetchPosts();
+
+    expect(posts.length, mockDocs.length);
+    expect(posts.first.thumbnail, mockDocs[0]['thumbnail']);
+    expect(posts.first.userId, mockDocs[0]['userId']);
+  });
+
+}
---
+++ test/features/users/data/repositories/user_repository_test.dart
@@ -0,0 +1,46 @@
+import 'package:blingpay_assesment/features/users/data/repositories/user_repository.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late UserRepository userRepository ;
+  late FakeFirebaseFirestore firebaseFireStore ;
+
+  setUp((){
+    firebaseFireStore = FakeFirebaseFirestore();
+    userRepository = UserRepository(firebaseFireStore: firebaseFireStore);
+  });
+
+  test('fetchUsers returns empty list when no documents found', () async {
+
+    final users = await userRepository.fetchUsers();
+    expect(users, isEmpty);
+  });
+
+  test('fetchUsers returns list of UserModel from documents', () async {
+    final List<Map<String, dynamic>> mockDocs = [
+      {
+        "userId": "2tJgiVZWQRczkDqqjzrG",
+        "username": "Test",
+        "name": " Brooklyn Simmons",
+        "photo": "https://firebasestorage.googleapis.com/vo/b/blynq-13e9a.appspot.com/o/default_user_img.png?alt=media&token=8190d4c3-8a93-4476-a37b-bc3faa74e71d, bio: Always curious to learn new stuff",
+        "bio": 'Always curious to learn new stuff'
+      }
+    ];
+
+    // Add data to fake Firestore collection
+    for (var doc in mockDocs) {
+      await firebaseFireStore.collection('users').add(doc);
+    }
+
+    // Call the method
+    final users = await userRepository.fetchUsers();
+
+    // Assert the result (similar to previous test)
+    expect(users.length, mockDocs.length);
+    expect(users.first.name, mockDocs[0]['name']);
+    expect(users.first.username, mockDocs[0]['username']);
+  });
+
+
+}
