69a1d9bfdd82154c498579601cf48bb1705143c4
Amos Godwin <amosgodwin500@gmail.com>
2024-07-02 12:31:21 +0100

    added env support to parse firebase cred

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:blingpay_assesment/app_colors.dart';
 import 'package:blingpay_assesment/features/users/presentation/pages/home.dart';
@@ -5,10 +7,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
+  Platform.isIOS
+      ? await Firebase.initializeApp()
+      : await Firebase.initializeApp(
+          options:  FirebaseOptions(
+              apiKey: dotenv.env['ANDROID_FIREBASE_API_KEY']!,
+              appId: dotenv.env['ANDROID_APP_ID']!,
+              messagingSenderId: dotenv.env['ANDROID_MESSEGNGER_SENDER_ID']!,
+              projectId: dotenv.env['PROJECT_ID']!));
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );
@@ -27,11 +38,7 @@ class MyApp extends StatelessWidget {
       initial: savedThemeMode ?? AdaptiveThemeMode.dark,
       light: ThemeData(
           // This is the light theme of the application.
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: veryDarkBlue,
-            onSurface: Colors.white,
-            outline: mysticWhite
-          ),
+          colorScheme: ColorScheme.fromSeed(seedColor: veryDarkBlue, onSurface: Colors.white, outline: mysticWhite),
           useMaterial3: true,
           textTheme: GoogleFonts.rubikTextTheme().copyWith(
             bodyMedium: const TextStyle(
@@ -40,7 +47,7 @@ class MyApp extends StatelessWidget {
           )),
       dark: ThemeData(
           // This is the dark theme of the application.
-          colorScheme: ColorScheme.fromSeed(seedColor: veryDarkBlue, onSurface: codGray,outline: mineShaftGrey),
+          colorScheme: ColorScheme.fromSeed(seedColor: veryDarkBlue, onSurface: codGray, outline: mineShaftGrey),
           useMaterial3: true,
           textTheme: GoogleFonts.rubikTextTheme().copyWith(
             bodyMedium: const TextStyle(
